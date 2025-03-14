d4120f201dc31ef2479316287e55b97ca311c52d
Zhouzw <122282628+Zzw131537@users.noreply.github.com>
2025-03-14 17:46:56 +0800

    UV统计

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -84,4 +84,21 @@ class HmDianPingApplicationTests {
          }
      }
 
+     @Test
+    void testHypterLogLog(){
+        String []users = new String[1000];
+
+        int index =0;
+        for(int i=1;i<=1000000;i++){
+            users[index++]="user_"+i;
+            if(i % 1000 == 0){
+                index =0;
+                stringRedisTemplate.opsForHyperLogLog().add("hll1",users);
+                // System.out.println("kk");
+            }
+        }
+
+        Long size = stringRedisTemplate.opsForHyperLogLog().size("hll1");
+         System.out.println("size  = "+size);
+     }
 }
